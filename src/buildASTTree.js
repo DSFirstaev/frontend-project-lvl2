e097edd7aa495a4eb5704ec4e67260f98d33ac40
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-08-24 21:19:46 +0300

    fix stylish

--- src/buildASTTree.js
+++ src/buildASTTree.js
@@ -2,9 +2,7 @@ import _ from 'lodash';
 
 export const buildASTTree = (data1, data2) => {
   const keys1 = _.keys(data1);
-  console.log(keys1);
   const keys2 = _.keys(data2);
-  console.log(keys2);
   const keys = _.union(keys1, keys2);
   const sortedKeys = _.sortBy(keys);
   const ASTTree = sortedKeys.map((key) => {
@@ -14,7 +12,7 @@ export const buildASTTree = (data1, data2) => {
     if (!_.has(data2, key)) {
       return { type: 'removed', key, value: data1[key] };
     }
-    if (_.isPlainObject(data1, key) && _.isPlainObject(data2, key)) {
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return { type: 'nested', key, children: buildASTTree(data1[key], data2[key]) };
     }
     if (!_.isEqual(data1[key], data2[key])) {
--- src/stylish.js
+++ src/stylish.js
@@ -1,24 +1,37 @@
-const makeGap = (depth, beginValue = 4) => ' '.repeat(beginValue * depth - 2);
+import _ from 'lodash';
+
+const makeIndent = (depth, beginValue = 4) => ' '.repeat(beginValue * depth - 2);
+
+const stringify = (data, depth) => {
+  if (_.isPlainObject(data) === false) {
+    return `${data}`;
+  }
+
+  const values = Object.entries(data)
+    .map(([key, value]) => `${makeIndent(depth + 1)}${key}: ${stringify(value, depth + 1)}`);
+  return ['{', ...values, `${makeIndent(depth)}}`].join('\n');
+};
+
+const makeLine = (depth, sign, key, value) => `${makeIndent(depth)}${sign} ${key}: ${stringify(value, depth)}`;
 
 const makeTree = (initialTree) => {
   const iter = (tree, depth) => tree.map((node) => {
-    const makeLine = (sign) => `${makeGap(depth)}${sign} ${node.key}: ${node.value}`;
     switch (node.type) {
       case 'added':
-        return makeLine('+', node.value);
+        return makeLine(depth, '+', node.key, node.value);
       case 'removed':
-        return makeLine('-', node.value);
+        return makeLine(depth, '-', node.key, node.value);
       case 'nested':
-        return `${makeGap(depth)}${node.key}: {\n${iter(node.children, depth + 1).join('\n')}\n${makeGap(depth)}}`;
+        return `${makeIndent(depth)}${node.key}: {\n${iter(node.children, depth + 1).join('\n')}\n${makeIndent(depth)}}`;
       case 'changed':
-        return `${makeLine('-', node.value1)}\n${makeLine('+', node.value2)}`;
+        return `${makeLine(depth, '-', node.key, node.value1)}\n${makeLine(depth, '+', node.key, node.value2)}`;
       case 'unchanged':
-        return makeLine(' ', node.value1);
+        return makeLine(depth, ' ', node.key, node.value);
       default:
         return new Error('This tree is bad. Try another tree');
     }
   });
-  return [`{\n${iter(initialTree, 1)}\n}`].join('\n');
+  return `{\n${iter(initialTree, 1).join('\n')}\n}`;
 };
 
 export default makeTree;

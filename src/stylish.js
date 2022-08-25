60fdad1f2ac27ff415bfa4ff86e9384628237012
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-08-25 16:55:34 +0300

    problem with indent

--- src/stylish.js
+++ src/stylish.js
@@ -7,26 +7,36 @@ const stringify = (data, depth) => {
     return `${data}`;
   }
 
+  if (data === null) {
+    return null;
+  }
+
   const values = Object.entries(data)
     .map(([key, value]) => `${makeIndent(depth + 1)}${key}: ${stringify(value, depth + 1)}`);
   return ['{', ...values, `${makeIndent(depth)}}`].join('\n');
 };
 
-const makeLine = (depth, sign, key, value) => `${makeIndent(depth)}${sign} ${key}: ${stringify(value, depth)}`;
+// const makeLine = (depth, sign, key, value) =>
+// `${makeIndent(depth)}${sign} ${key}: ${stringify(value, depth)}`;
 
 const makeTree = (initialTree) => {
   const iter = (tree, depth) => tree.map((node) => {
     switch (node.type) {
       case 'added':
-        return makeLine(depth, '+', node.key, node.value);
+        // return makeLine(depth, '+', node.key, node.value);
+        return `    ${'+'} ${node.key}: ${stringify(node.value, depth)}`;
       case 'removed':
-        return makeLine(depth, '-', node.key, node.value);
+        // return makeLine(depth, '-', node.key, node.value);
+        return `    ${'-'} ${node.key}: ${stringify(node.value, depth)}`;
       case 'nested':
         return `${makeIndent(depth)}${node.key}: {\n${iter(node.children, depth + 1).join('\n')}\n${makeIndent(depth)}}`;
       case 'changed':
-        return `${makeLine(depth, '-', node.key, node.value1)}\n${makeLine(depth, '+', node.key, node.value2)}`;
+        // return `${makeLine(depth, '-', node.key, node.value1)}\n
+        // ${makeLine(depth, '+', node.key, node.value2)}`;
+        return `    ${'-'} ${node.key}: ${stringify(node.value1, depth)}\n    ${'+'} ${node.key}: ${stringify(node.value2, depth)}`;
       case 'unchanged':
-        return makeLine(depth, ' ', node.key, node.value);
+        // return makeLine(depth, ' ', node.key, node.value);
+        return `    ${' '} ${node.key}: ${stringify(node.value, depth)}`;
       default:
         return new Error('This tree is bad. Try another tree');
     }

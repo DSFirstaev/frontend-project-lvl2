432a0c98fba28dd3a5c702c6d237e4e26f8add24
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-08-26 15:12:36 +0300

    default stylish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format stylish', 'output format', 'stylish')
   .argument('<filepath1>', 'path to file1')
   .argument('<filepath2>', 'path to file2')
   .action((filepath1, filepath2) => {
